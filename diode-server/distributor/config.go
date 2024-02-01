1c3651b74269784615b247a7d212f6e4fcdd7e63
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2024-02-01 19:32:04 +0000

    feat: OBS-381 - set correct default redis port
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

--- diode-server/distributor/config.go
+++ diode-server/distributor/config.go
@@ -4,6 +4,6 @@ package distributor
 type Config struct {
 	GRPCPort      int    `envconfig:"GRPC_PORT" default:"8081"`
 	RedisHost     string `envconfig:"REDIS_HOST" default:"127.0.0.1"`
-	RedisPort     string `envconfig:"REDIS_PORT" default:"6378"`
+	RedisPort     string `envconfig:"REDIS_PORT" default:"6379"`
 	RedisPassword string `envconfig:"REDIS_PASSWORD" required:"true"`
 }
