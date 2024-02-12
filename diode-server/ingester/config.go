5d47b6247a92adc2f575cc4e721c5e5f63503447
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2024-02-12 14:01:48 +0000

    feat: OBS-386 - ingester: add redis configuration
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

--- diode-server/ingester/config.go
+++ diode-server/ingester/config.go
@@ -2,4 +2,7 @@ package ingester
 
 // Config is the configuration for the ingester service
 type Config struct {
+	RedisHost     string `envconfig:"REDIS_HOST" default:"127.0.0.1"`
+	RedisPort     string `envconfig:"REDIS_PORT" default:"6379"`
+	RedisPassword string `envconfig:"REDIS_PASSWORD" required:"true"`
 }
