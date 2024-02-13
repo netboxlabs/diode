b546ac13079084ae18c0661ac81aacc66046a530
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2024-02-13 14:19:07 +0000

    feat: OBS-386 - redis FT indices work only on DB 0
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

--- diode-server/ingester/config.go
+++ diode-server/ingester/config.go
@@ -6,5 +6,5 @@ type Config struct {
 	RedisPort     string `envconfig:"REDIS_PORT" default:"6379"`
 	RedisPassword string `envconfig:"REDIS_PASSWORD" required:"true"`
 	RedisStreamDB int    `envconfig:"REDIS_STREAM_DB" default:"0"`
-	RedisDB       int    `envconfig:"REDIS_DB" default:"1"`
+	RedisDB       int    `envconfig:"REDIS_DB" default:"0"`
 }
