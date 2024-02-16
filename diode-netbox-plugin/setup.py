f44a172eddde9dd414dd8081a3e18d043f30d747
Julio-Oliveira-Encora <julioh.oliveira@encora.com>
2024-02-16 12:46:50 -0300

    Added some files to plugin api

---
+++ diode-netbox-plugin/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netboxlabs-diode-netbox-plugin',
+    version='0.0.1',
+    description='NetBox Labs, Diode Netbox plugin',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
