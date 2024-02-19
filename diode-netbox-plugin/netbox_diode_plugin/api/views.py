364f5e215c17c3b6e48d65a5bd19ef90cba638c7
Julio-Oliveira-Encora <julioh.oliveira@encora.com>
2024-02-19 17:32:57 -0300

    -Added views, serializers and url for diode-netbox-plugin. - Some tests with parameters.

--- diode-netbox-plugin/netbox_diode_plugin/api/views.py
+++ diode-netbox-plugin/netbox_diode_plugin/api/views.py
@@ -15,20 +15,6 @@ class ObjectStateList(generics.ListAPIView):
     queryset = CachedValue.objects.all()
 
     def get_queryset(self):
-        """
-        Get the list of items for this view.
-        This must be an iterable, and may be a queryset.
-        Defaults to using `self.queryset`.
-
-        This method should always be used rather than accessing `self.queryset`
-        directly, as `self.queryset` gets evaluated only once, and those results
-        are cached for all subsequent requests.
-
-        You may want to override this if you need to provide different
-        querysets depending on the incoming request.
-
-        (Eg. return a list of items that is specific to the user)
-        """
 
         # Restrict results by object type
         # object_types = []
