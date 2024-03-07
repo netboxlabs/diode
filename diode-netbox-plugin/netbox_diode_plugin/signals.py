f29a0b3c6aee50cd48a1d0a7abd07ce6d585ac58
Julio-Oliveira-Encora <julioh.oliveira@encora.com>
2024-03-07 15:29:42 -0300

    Initial development. Added method "handle_notifify_reconciliation" to receive "post_save" signal.

--- diode-netbox-plugin/netbox_diode_plugin/signals.py
+++ diode-netbox-plugin/netbox_diode_plugin/signals.py
@@ -3,23 +3,22 @@
 """Diode Netbox Plugin - Signals."""
 
 
+import json
 import logging
 
 from django.contrib.contenttypes.models import ContentType
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.forms import model_to_dict
-
 from extras.models import ObjectChange
-from dcim.models import Device
+
 from netbox_diode_plugin.diode_reconciler_sdk.client import DiodeReconcilerClient
 
 logger = logging.getLogger("netbox.netbox_diode_plugin")
 
 
 @receiver(post_save)
-def handle_notifify_reconciliation(instance, created, sender, update_fields, **kwargs):
+def handle_notify_diode(instance, created, sender, update_fields, **kwargs):
     """Handle notify reconciliation."""
     logger.debug("Handling notify reconciliation.")
 
@@ -36,14 +35,18 @@ def handle_notifify_reconciliation(instance, created, sender, update_fields, **k
         object_id = instance.id
         object_type = f"{app_label}.{model_name}"
         object_changed_id = object_changed.id
-        object = model_to_dict(instance)
+        object = {model_name: model_to_dict(instance)}
 
-        # print(object_id, object_type, object_changed_id, object)
+        # Comment out because the DiodeReconcilerClient need some adjustments.
 
-        sdk = DiodeReconcilerClient("localhost:50051", "foobar")
-        sdk.add_object_state(
-            object_id=object_id,
-            object_type=object_type,
-            object_change_id=object_changed_id,
-            # object=object,
-        )
+        # sdk = DiodeReconcilerClient(
+        #     # "0.0.0.0:8082",
+        #     "diode-reconciler:8081",
+        #     "api-key" "1e99338b8cab5fc637bc55f390bda1446f619c42",
+        # )
+        # sdk.add_object_state(
+        #     object_id=object_id,
+        #     object_type=object_type,
+        #     object_change_id=object_changed_id,
+        #     object=object,
+        # )
