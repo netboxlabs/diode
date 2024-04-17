ee3832b01389165ecd28666b47a2e0a00fbadfd3
Julio-Oliveira-Encora <julioh.oliveira@encora.com>
2024-04-17 11:06:02 -0300

    Cleanup updated

--- tests/features/cleanup.feature
+++ tests/features/cleanup.feature
@@ -5,3 +5,13 @@ Feature: Cleanup tests objects
 Scenario: Cleanup of site object
     Given the site object "Site A" is deleted
     Then the site object is removed from the database
+
+@cleanup
+Scenario: Cleanup of device role object
+    Given the device role object "WAN Router" is deleted
+    Then the device role object is removed from the database
+
+@cleanup
+Scenario: Cleanup of manufacturer object
+    Given the manufacturer object "Cisco" is deleted
+    Then the manufacturer object is removed from the database
--- tests/features/steps/cleanup.py
+++ tests/features/steps/cleanup.py
@@ -21,3 +21,39 @@ def check_site_object_deleted(context):
     endpoint = "dcim/sites/"
     site = get_object_by_name(context.site_name, endpoint)
     assert site is None
+
+
+@given('the device role object "{device_role_name}" is deleted')
+def delete_device_role_object(context, device_role_name):
+    """Delete the device role object with the given name."""
+    context.device_role_name = device_role_name
+    endpoint = "dcim/device-roles"
+    device_role = get_object_by_name(context.device_role_name, endpoint)
+    if device_role:
+        send_delete_request(endpoint, device_role.get("id"))
+
+
+@then("the device role object is removed from the database")
+def check_device_role_object_deleted(context):
+    """Check if the device role object is removed from the database."""
+    endpoint = "dcim/device-roles"
+    device_role = get_object_by_name(context.device_role_name, endpoint)
+    assert device_role is None
+
+
+@given('the manufacturer object "{manufacturer_name}" is deleted')
+def delete_manufacturer_object(context, manufacturer_name):
+    """Delete the manufacturer object with the given name."""
+    context.manufacturer_name = manufacturer_name
+    endpoint = "dcim/manufacturers"
+    manufacturer = get_object_by_name(context.manufacturer_name, endpoint)
+    if manufacturer:
+        send_delete_request(endpoint, manufacturer.get("id"))
+
+
+@then("the manufacturer object is removed from the database")
+def check_manufacturer_object_deleted(context):
+    """Check if the manufacturer object is removed from the database."""
+    endpoint = "dcim/manufacturers"
+    manufacturer = get_object_by_name(context.manufacturer_name, endpoint)
+    assert manufacturer is None
