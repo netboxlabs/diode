205afa08b3317503dc39c92955e0c338394c37cf
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2024-05-02 17:43:28 +0100

    feat: OBS-430 - diode-sdk-go: add prefix (IPAM)
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

--- diode-sdk-go/diode/v1/diodepb/ingester.pb.validate.go
+++ diode-sdk-go/diode/v1/diodepb/ingester.pb.validate.go
@@ -429,6 +429,47 @@ func (m *Entity) validate(all bool) error {
 			}
 		}
 
+	case *Entity_Prefix:
+		if v == nil {
+			err := EntityValidationError{
+				field:  "Entity",
+				reason: "oneof value cannot be a typed-nil",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+		if all {
+			switch v := interface{}(m.GetPrefix()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, EntityValidationError{
+						field:  "Prefix",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, EntityValidationError{
+						field:  "Prefix",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetPrefix()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return EntityValidationError{
+					field:  "Prefix",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
 	default:
 		_ = v // ensures v is used
 	}
