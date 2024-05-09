512a79b3dd33889810f6fd9f3ec754ac8852e697
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2024-05-09 14:57:39 +0100

    feat: OBS-435 - diode-sdk-python: fix client test
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

--- diode-sdk-python/tests/test_client.py
+++ diode-sdk-python/tests/test_client.py
@@ -11,7 +11,12 @@ from netboxlabs.diode.sdk.exceptions import DiodeClientError, DiodeConfigError
 
 def test_init():
     """Ensure we can initiate a client configuration."""
-    config = DiodeClient(target="localhost:8081", app_name="my-producer", app_version="0.0.1", api_key="abcde")
+    config = DiodeClient(
+        target="localhost:8081",
+        app_name="my-producer",
+        app_version="0.0.1",
+        api_key="abcde",
+    )
     assert config.target == "localhost:8081"
     assert config.name == "diode-sdk-python"
     assert config.version == "0.0.1"
@@ -22,13 +27,23 @@ def test_init():
 def test_config_error():
     """Ensure we can raise a config error."""
     with pytest.raises(DiodeConfigError) as err:
-        DiodeClient(target="localhost:8081", app_name="my-producer", app_version="0.0.1")
-    assert str(err.value) == "API key is required"
+        DiodeClient(
+            target="localhost:8081", app_name="my-producer", app_version="0.0.1"
+        )
+    assert (
+        str(err.value)
+        == f"api_key param or DIODE_API_KEY environment variable required"
+    )
 
 
 def test_client_error():
     """Ensure we can raise a client error."""
     with pytest.raises(DiodeClientError) as err:
-        client = DiodeClient(target="invalid:8081", app_name="my-producer", app_version="0.0.1", api_key="abcde")
+        client = DiodeClient(
+            target="invalid:8081",
+            app_name="my-producer",
+            app_version="0.0.1",
+            api_key="abcde",
+        )
         client.ingest(entities=[])
     assert err.value.status_code == grpc.StatusCode.UNAVAILABLE
