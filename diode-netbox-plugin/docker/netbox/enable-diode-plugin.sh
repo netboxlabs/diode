85901a52967c9eacc92d96cef4158af0da006b93
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2024-07-17 17:47:54 -0300

    chore: diode netbox plugin + gha cleanup

--- diode-netbox-plugin/docker/netbox/enable-diode-plugin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Enabling diode-netbox-plugin"
-
-echo "PLUGINS = [\"netbox_diode_plugin\"]" > /etc/netbox/config/plugins.py
-
-./manage.py configurediodeplugin || echo "❌ enabling diode-netbox-plugin failed" && exit 1
-
-echo "✅ diode-netbox-plugin enabled successfully!"
--- diode-netbox-plugin/netbox_diode_plugin/tables.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# Copyright 2024 NetBox Labs Inc
-"""Diode Netbox Plugin - Tables."""
-
-from django_tables2 import tables
