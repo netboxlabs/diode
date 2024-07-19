091703b951197ee02a64dc6ca4359437a39819c8
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2024-07-19 15:33:26 -0300

    lint fix

--- diode-server/netboxdiodeplugin/client.go
+++ diode-server/netboxdiodeplugin/client.go
@@ -32,8 +32,8 @@ const (
 	// BaseURLEnvVarName is the environment variable name for the NetBox Diode plugin HTTP base URL
 	BaseURLEnvVarName = "NETBOX_DIODE_PLUGIN_API_BASE_URL"
 
-	// TlsSkipVerify is the environment variable name for Netbox Diode plugin TLS verification
-	TlsSkipVerify = "NETBOX_DIODE_PLUGIN_SKIP_TLS_VERIFY"
+	// TLSSkipVerify is the environment variable name for Netbox Diode plugin TLS verification
+	TLSSkipVerify = "NETBOX_DIODE_PLUGIN_SKIP_TLS_VERIFY"
 
 	// TimeoutSecondsEnvVarName is the environment variable name for the NetBox Diode plugin HTTP timeout
 	TimeoutSecondsEnvVarName = "NETBOX_DIODE_PLUGIN_API_TIMEOUT_SECONDS"
@@ -160,7 +160,7 @@ func baseURL() string {
 }
 
 func tlsConfig() bool {
-	skipTLS, ok := os.LookupEnv(TlsSkipVerify)
+	skipTLS, ok := os.LookupEnv(TLSSkipVerify)
 	if !ok {
 		return false
 	}
