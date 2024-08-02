d6ac9d28702e9188ae4053eaaecd1b0130c37cbd
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2024-08-02 09:03:06 +0100

    feat: docker - remove redundant configurediodeplugin command call
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

--- diode-server/docker/netbox/configure-diode-plugin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "🎛️ Configuring diode-netbox-plugin"
-
-echo "PLUGINS = [\"netbox_diode_plugin\"]" > /etc/netbox/config/plugins.py
-
-./manage.py configurediodeplugin || { echo "❌ enabling diode-netbox-plugin failed"; exit 1; }
-
-echo "✅ diode-netbox-plugin configured successfully!"
