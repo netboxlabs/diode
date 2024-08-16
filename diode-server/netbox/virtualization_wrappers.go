69e89e0e059164c69d53397fb806f8b92d262bb6
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2024-08-16 11:21:22 -0300

    fix nestedobject code

--- diode-server/netbox/virtualization_wrappers.go
+++ diode-server/netbox/virtualization_wrappers.go
@@ -1,6 +1,12 @@
 package netbox
 
-import "github.com/gosimple/slug"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gosimple/slug"
+	"github.com/mitchellh/hashstructure/v2"
+)
 
 // VirtualizationClusterGroupDataWrapper represents a virtualization cluster group data wrapper
 type VirtualizationClusterGroupDataWrapper struct {
@@ -107,6 +113,52 @@ func (vw *VirtualizationClusterGroupDataWrapper) IsPlaceholder() bool {
 
 // Patch creates patches between the actual, intended and current data
 func (vw *VirtualizationClusterGroupDataWrapper) Patch(cmp ComparableData, intendedNestedObjects map[string]ComparableData) ([]ComparableData, error) {
+	intended, ok := cmp.(*VirtualizationClusterGroupDataWrapper)
+
+	if !ok && intended != nil {
+		return nil, errors.New("invalid data type")
+	}
+
+	reconciliationRequired := true
+
+	if intended != nil {
+		vw.ClusterGroup.ID = intended.ClusterGroup.ID
+		vw.ClusterGroup.Name = intended.ClusterGroup.Name
+		vw.ClusterGroup.Slug = intended.ClusterGroup.Slug
+
+		if vw.ClusterGroup.Description == nil {
+			vw.ClusterGroup.Description = intended.ClusterGroup.Description
+		}
+
+		tagsToMerge := mergeTags(vw.ClusterGroup.Tags, intended.ClusterGroup.Tags, intendedNestedObjects)
+
+		if len(tagsToMerge) > 0 {
+			vw.ClusterGroup.Tags = tagsToMerge
+		}
+
+		actualHash, _ := hashstructure.Hash(vw.Data(), hashstructure.FormatV2, nil)
+		intendedHash, _ := hashstructure.Hash(intended.Data(), hashstructure.FormatV2, nil)
+
+		reconciliationRequired = actualHash != intendedHash
+	} else {
+		tagsToMerge := mergeTags(vw.ClusterGroup.Tags, nil, intendedNestedObjects)
+
+		if len(tagsToMerge) > 0 {
+			vw.ClusterGroup.Tags = tagsToMerge
+		}
+	}
+
+	for _, t := range vw.ClusterGroup.Tags {
+		if t.ID == 0 {
+			vw.objectsToReconcile = append(vw.objectsToReconcile, &TagDataWrapper{Tag: t, hasParent: true})
+		}
+	}
+
+	if reconciliationRequired {
+		vw.hasChanged = true
+		vw.objectsToReconcile = append(vw.objectsToReconcile, vw)
+	}
+
 	return vw.objectsToReconcile, nil
 }
 
@@ -218,6 +270,52 @@ func (vw *VirtualizationClusterTypeDataWrapper) IsPlaceholder() bool {
 
 // Patch creates patches between the actual, intended and current data
 func (vw *VirtualizationClusterTypeDataWrapper) Patch(cmp ComparableData, intendedNestedObjects map[string]ComparableData) ([]ComparableData, error) {
+	intended, ok := cmp.(*VirtualizationClusterTypeDataWrapper)
+
+	if !ok && intended != nil {
+		return nil, errors.New("invalid data type")
+	}
+
+	reconciliationRequired := true
+
+	if intended != nil {
+		vw.ClusterType.ID = intended.ClusterType.ID
+		vw.ClusterType.Name = intended.ClusterType.Name
+		vw.ClusterType.Slug = intended.ClusterType.Slug
+
+		if vw.ClusterType.Description == nil {
+			vw.ClusterType.Description = intended.ClusterType.Description
+		}
+
+		tagsToMerge := mergeTags(vw.ClusterType.Tags, intended.ClusterType.Tags, intendedNestedObjects)
+
+		if len(tagsToMerge) > 0 {
+			vw.ClusterType.Tags = tagsToMerge
+		}
+
+		actualHash, _ := hashstructure.Hash(vw.Data(), hashstructure.FormatV2, nil)
+		intendedHash, _ := hashstructure.Hash(intended.Data(), hashstructure.FormatV2, nil)
+
+		reconciliationRequired = actualHash != intendedHash
+	} else {
+		tagsToMerge := mergeTags(vw.ClusterType.Tags, nil, intendedNestedObjects)
+
+		if len(tagsToMerge) > 0 {
+			vw.ClusterType.Tags = tagsToMerge
+		}
+	}
+
+	for _, t := range vw.ClusterType.Tags {
+		if t.ID == 0 {
+			vw.objectsToReconcile = append(vw.objectsToReconcile, &TagDataWrapper{Tag: t, hasParent: true})
+		}
+	}
+
+	if reconciliationRequired {
+		vw.hasChanged = true
+		vw.objectsToReconcile = append(vw.objectsToReconcile, vw)
+	}
+
 	return vw.objectsToReconcile, nil
 }
 
@@ -280,6 +378,39 @@ func (vw *VirtualizationClusterDataWrapper) NestedObjects() ([]ComparableData, e
 		vw.placeholder = true
 	}
 
+	clusterGroup := VirtualizationClusterGroupDataWrapper{ClusterGroup: vw.Cluster.Group, placeholder: vw.placeholder, hasParent: true, intended: vw.intended}
+
+	cgo, err := clusterGroup.NestedObjects()
+	if err != nil {
+		return nil, err
+	}
+
+	objects = append(objects, cgo...)
+
+	vw.Cluster.Group = clusterGroup.ClusterGroup
+
+	clusterType := VirtualizationClusterTypeDataWrapper{ClusterType: vw.Cluster.Type, placeholder: vw.placeholder, hasParent: true, intended: vw.intended}
+
+	cto, err := clusterGroup.NestedObjects()
+	if err != nil {
+		return nil, err
+	}
+
+	objects = append(objects, cto...)
+
+	vw.Cluster.Type = clusterType.ClusterType
+
+	site := DcimSiteDataWrapper{Site: vw.Cluster.Site, placeholder: vw.placeholder, hasParent: true, intended: vw.intended}
+
+	so, err := site.NestedObjects()
+	if err != nil {
+		return nil, err
+	}
+
+	objects = append(objects, so...)
+
+	vw.Cluster.Site = site.Site
+
 	if vw.Cluster.Tags != nil {
 		for _, t := range vw.Cluster.Tags {
 			if t.Slug == "" {
@@ -325,6 +456,54 @@ func (vw *VirtualizationClusterDataWrapper) IsPlaceholder() bool {
 
 // Patch creates patches between the actual, intended and current data
 func (vw *VirtualizationClusterDataWrapper) Patch(cmp ComparableData, intendedNestedObjects map[string]ComparableData) ([]ComparableData, error) {
+	intended, ok := cmp.(*DcimManufacturerDataWrapper)
+
+	if !ok && intended != nil {
+		return nil, errors.New("invalid data type")
+	}
+
+	actualDevice := extractFromObjectsMap(actualNestedObjectsMap, fmt.Sprintf("%p", dw.Interface.Device))
+	intendedDevice := extractFromObjectsMap(intendedNestedObjects, fmt.Sprintf("%p", dw.Interface.Device))
+
+	reconciliationRequired := true
+
+	if intended != nil {
+		vw.Cluster.ID = intended.Manufacturer.ID
+		vw.Cluster.Name = intended.Manufacturer.Name
+
+		if vw.Cluster.Description == nil {
+			vw.Cluster.Description = intended.Manufacturer.Description
+		}
+
+		tagsToMerge := mergeTags(vw.Cluster.Tags, intended.Manufacturer.Tags, intendedNestedObjects)
+
+		if len(tagsToMerge) > 0 {
+			vw.Cluster.Tags = tagsToMerge
+		}
+
+		actualHash, _ := hashstructure.Hash(vw.Data(), hashstructure.FormatV2, nil)
+		intendedHash, _ := hashstructure.Hash(intended.Data(), hashstructure.FormatV2, nil)
+
+		reconciliationRequired = actualHash != intendedHash
+	} else {
+		tagsToMerge := mergeTags(vw.Cluster.Tags, nil, intendedNestedObjects)
+
+		if len(tagsToMerge) > 0 {
+			vw.Cluster.Tags = tagsToMerge
+		}
+	}
+
+	for _, t := range vw.Cluster.Tags {
+		if t.ID == 0 {
+			vw.objectsToReconcile = append(vw.objectsToReconcile, &TagDataWrapper{Tag: t, hasParent: true})
+		}
+	}
+
+	if reconciliationRequired {
+		vw.hasChanged = true
+		vw.objectsToReconcile = append(vw.objectsToReconcile, vw)
+	}
+
 	return vw.objectsToReconcile, nil
 }
 
@@ -387,6 +566,67 @@ func (vw *VirtualizationVirtualMachineDataWrapper) NestedObjects() ([]Comparable
 		vw.placeholder = true
 	}
 
+	// Ignore primary IP addresses for time being
+	vw.VirtualMachine.PrimaryIPv4 = nil
+	vw.VirtualMachine.PrimaryIPv6 = nil
+
+	cluster := VirtualizationClusterDataWrapper{Cluster: vw.VirtualMachine.Cluster, placeholder: vw.placeholder, hasParent: true, intended: vw.intended}
+
+	co, err := cluster.NestedObjects()
+	if err != nil {
+		return nil, err
+	}
+
+	objects = append(objects, co...)
+
+	vw.VirtualMachine.Cluster = cluster.Cluster
+
+	site := DcimSiteDataWrapper{Site: vw.VirtualMachine.Site, placeholder: vw.placeholder, hasParent: true, intended: vw.intended}
+
+	so, err := site.NestedObjects()
+	if err != nil {
+		return nil, err
+	}
+
+	objects = append(objects, so...)
+
+	vw.VirtualMachine.Site = site.Site
+
+	if vw.VirtualMachine.Platform != nil {
+		platform := DcimPlatformDataWrapper{Platform: vw.VirtualMachine.Platform, placeholder: vw.placeholder, hasParent: true, intended: vw.intended}
+
+		po, err := platform.NestedObjects()
+		if err != nil {
+			return nil, err
+		}
+
+		objects = append(objects, po...)
+
+		vw.VirtualMachine.Platform = platform.Platform
+	}
+
+	deviceRole := DcimDeviceRoleDataWrapper{DeviceRole: vw.VirtualMachine.Role, placeholder: vw.placeholder, hasParent: true, intended: vw.intended}
+
+	dro, err := deviceRole.NestedObjects()
+	if err != nil {
+		return nil, err
+	}
+
+	objects = append(objects, dro...)
+
+	vw.VirtualMachine.Role = deviceRole.DeviceRole
+
+	device := DcimDeviceDataWrapper{Device: vw.VirtualMachine.Device, placeholder: vw.placeholder, hasParent: true, intended: vw.intended}
+
+	do, err := device.NestedObjects()
+	if err != nil {
+		return nil, err
+	}
+
+	objects = append(objects, do...)
+
+	vw.VirtualMachine.Device = device.Device
+
 	if vw.VirtualMachine.Tags != nil {
 		for _, t := range vw.VirtualMachine.Tags {
 			if t.Slug == "" {
@@ -432,6 +672,51 @@ func (vw *VirtualizationVirtualMachineDataWrapper) IsPlaceholder() bool {
 
 // Patch creates patches between the actual, intended and current data
 func (vw *VirtualizationVirtualMachineDataWrapper) Patch(cmp ComparableData, intendedNestedObjects map[string]ComparableData) ([]ComparableData, error) {
+	intended, ok := cmp.(*DcimManufacturerDataWrapper)
+
+	if !ok && intended != nil {
+		return nil, errors.New("invalid data type")
+	}
+
+	reconciliationRequired := true
+
+	if intended != nil {
+		vw.VirtualMachine.ID = intended.Manufacturer.ID
+		vw.VirtualMachine.Name = intended.Manufacturer.Name
+
+		if vw.VirtualMachine.Description == nil {
+			vw.VirtualMachine.Description = intended.Manufacturer.Description
+		}
+
+		tagsToMerge := mergeTags(vw.VirtualMachine.Tags, intended.Manufacturer.Tags, intendedNestedObjects)
+
+		if len(tagsToMerge) > 0 {
+			vw.VirtualMachine.Tags = tagsToMerge
+		}
+
+		actualHash, _ := hashstructure.Hash(vw.Data(), hashstructure.FormatV2, nil)
+		intendedHash, _ := hashstructure.Hash(intended.Data(), hashstructure.FormatV2, nil)
+
+		reconciliationRequired = actualHash != intendedHash
+	} else {
+		tagsToMerge := mergeTags(vw.VirtualMachine.Tags, nil, intendedNestedObjects)
+
+		if len(tagsToMerge) > 0 {
+			vw.VirtualMachine.Tags = tagsToMerge
+		}
+	}
+
+	for _, t := range vw.VirtualMachine.Tags {
+		if t.ID == 0 {
+			vw.objectsToReconcile = append(vw.objectsToReconcile, &TagDataWrapper{Tag: t, hasParent: true})
+		}
+	}
+
+	if reconciliationRequired {
+		vw.hasChanged = true
+		vw.objectsToReconcile = append(vw.objectsToReconcile, vw)
+	}
+
 	return vw.objectsToReconcile, nil
 }
 
