1715747a09f2c65aa829733b5f58144ad25faa22
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2024-08-30 12:28:14 +0100

    add unit tests for authorization check
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

---
+++ diode-server/reconciler/server_internal_test.go
@@ -0,0 +1,65 @@
+package reconciler
+
+import (
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/netboxlabs/diode/diode-server/reconciler/v1/reconcilerpb"
+)
+
+func TestIsAuthorized(t *testing.T) {
+	tests := []struct {
+		name          string
+		rpcMethod     string
+		authorization []string
+		apiKeys       map[string]string
+		isAuthorized  bool
+	}{
+		{
+			name:          "missing authorization header",
+			rpcMethod:     reconcilerpb.ReconcilerService_RetrieveIngestionDataSources_FullMethodName,
+			authorization: []string{},
+			apiKeys: map[string]string{
+				"INGESTER_TO_RECONCILER": "test",
+			},
+			isAuthorized: false,
+		},
+		{
+			name:          "retrieve ingestion data sources with valid authorization",
+			rpcMethod:     reconcilerpb.ReconcilerService_RetrieveIngestionDataSources_FullMethodName,
+			authorization: []string{"test"},
+			apiKeys: map[string]string{
+				"INGESTER_TO_RECONCILER": "test",
+			},
+			isAuthorized: true,
+		},
+		{
+			name:          "retrieve ingestion data sources with invalid authorization",
+			rpcMethod:     reconcilerpb.ReconcilerService_RetrieveIngestionDataSources_FullMethodName,
+			authorization: []string{"test0"},
+			apiKeys: map[string]string{
+				"INGESTER_TO_RECONCILER": "test",
+			},
+			isAuthorized: false,
+		},
+		{
+			name:          "retrieve ingestion data sources for server without api key configured",
+			rpcMethod:     reconcilerpb.ReconcilerService_RetrieveIngestionDataSources_FullMethodName,
+			authorization: []string{"test"},
+			apiKeys: map[string]string{
+				"DIODE": "foorbar",
+			},
+			isAuthorized: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+			assert.Equal(t, tt.isAuthorized, isAuthorized(logger, tt.rpcMethod, tt.apiKeys, tt.authorization))
+		})
+	}
+}
