9c43b5530960a0695b0d6a4493098cf4a0538db2
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2024-09-10 10:31:29 -0300

    Increase test coverage

--- diode-server/reconciler/server_internal_test.go
+++ diode-server/reconciler/server_internal_test.go
@@ -2,6 +2,7 @@ package reconciler
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 	"os"
 	"testing"
@@ -112,6 +113,7 @@ func TestRetrieveLogs(t *testing.T) {
 		in       reconcilerpb.RetrieveIngestionLogsRequest
 		result   interface{}
 		response *reconcilerpb.RetrieveIngestionLogsResponse
+		failCmd  bool
 		hasError bool
 	}{
 		{
@@ -170,6 +172,7 @@ func TestRetrieveLogs(t *testing.T) {
 				},
 				NextPageToken: "F/Jk/zc08gA=",
 			},
+			failCmd:  false,
 			hasError: false,
 		},
 		{
@@ -181,7 +184,7 @@ func TestRetrieveLogs(t *testing.T) {
 				"results": []interface{}{
 					map[interface{}]interface{}{
 						"extra_attributes": map[interface{}]interface{}{
-							"$":            `{"data_type":"ipam.ipaddress","entity":{"IpAddress":{"address":"192.168.1.1","AssignedObject":null,"description":"Vendor: HUAWEI TECHNOLOGIES"}},"error":{"Message":"failed to apply change set","Code":400,"Details":{"change_set_id":"6304c706-f955-4bcb-a1cc-514293d53d07","result":"failed","errors":[{"address":"address: Duplicate IP address found in global table: 192.168.1.1/32","change_id":"ff9e29b2-7a64-40ba-99a8-21f44768f60a"}]}},"ingestion_ts":1725046967777525928,"producer_app_name":"example-app","producer_app_version":"0.1.0","request_id":"e03c4892-5b7e-4c39-b5e6-0225a264ab8b","sdk_name":"diode-sdk-go","sdk_version":"0.1.0","state":2}`,
+							"$":            `{"data_type":"ipam.ipaddress","entity":{"IpAddress":{"address":"192.168.1.1","AssignedObject":null,"description":"Vendor: HUAWEI TECHNOLOGIES"}},"error":{"Message":"failed to apply change set","Code":400,"Details":{"change_set_id":"6304c706-f955-4bcb-a1cc-514293d53d07","result":"failed","errors":[{"error":"address: Duplicate IP address found in global table: 192.168.1.1/32","change_id":"ff9e29b2-7a64-40ba-99a8-21f44768f60a"}]}},"ingestion_ts":1725046967777525928,"producer_app_name":"example-app","producer_app_version":"0.1.0","request_id":"e03c4892-5b7e-4c39-b5e6-0225a264ab8b","sdk_name":"diode-sdk-go","sdk_version":"0.1.0","state":2}`,
 							"ingestion_ts": "1725552914392208640",
 						},
 						"id":     "ingest-entity:ipam.ipaddress-1725046967777525928-5e272f66-61a9-4142-8013-a7aadb7264a8",
@@ -212,7 +215,7 @@ func TestRetrieveLogs(t *testing.T) {
 								Errors: []*reconcilerpb.ChangeSetError_Details_Error{
 									{
 										ChangeId: "ff9e29b2-7a64-40ba-99a8-21f44768f60a",
-										Error:    "",
+										Error:    "address: Duplicate IP address found in global table: 192.168.1.1/32",
 									},
 								},
 							},
@@ -221,7 +224,242 @@ func TestRetrieveLogs(t *testing.T) {
 				},
 				NextPageToken: "F/JlO7d81QA=",
 			},
+			failCmd:  false,
 			hasError: false,
+		},
+		{
+			name: "filter by state",
+			in:   reconcilerpb.RetrieveIngestionLogsRequest{State: reconcilerpb.State_RECONCILED.Enum()},
+			result: interface{}(map[interface{}]interface{}{
+				"attributes": []interface{}{},
+				"format":     "STRING",
+				"results": []interface{}{
+					map[interface{}]interface{}{
+						"extra_attributes": map[interface{}]interface{}{
+							"$":            `{"data_type":"dcim.interface","entity":{"Interface":{"device":{"name":"my_dev"},"name":"Gig 2"}},"ingestion_ts":1725552914392208722,"producer_app_name":"diode-agent","producer_app_version":"0.0.1","request_id":"req-id","sdk_name":"diode-sdk-go","sdk_version":"0.1.0","state":1}`,
+							"ingestion_ts": "1725552914392208640",
+						},
+						"id":     "ingest-entity:dcim.interface-1725552914392208722-db0931ec-c119-4702-bd74-4f0bed4e110b",
+						"values": []interface{}{},
+					},
+				},
+				"total_results": 1,
+				"warning":       []interface{}{},
+			}),
+			response: &reconcilerpb.RetrieveIngestionLogsResponse{
+				Logs: []*reconcilerpb.IngestionLog{
+					{
+						DataType:           "dcim.interface",
+						State:              reconcilerpb.State_RECONCILED,
+						RequestId:          "req-id",
+						IngestionTs:        1725552914392208722,
+						ProducerAppName:    "diode-agent",
+						ProducerAppVersion: "0.0.1",
+						SdkName:            "diode-sdk-go",
+						SdkVersion:         "0.1.0",
+						Entity:             &diodepb.Entity{},
+						Error:              nil,
+					},
+				},
+				NextPageToken: "F/JlO7d81QA=",
+			},
+			failCmd:  false,
+			hasError: false,
+		},
+		{
+			name: "filter by data type",
+			in:   reconcilerpb.RetrieveIngestionLogsRequest{DataType: "dcim.interface"},
+			result: interface{}(map[interface{}]interface{}{
+				"attributes": []interface{}{},
+				"format":     "STRING",
+				"results": []interface{}{
+					map[interface{}]interface{}{
+						"extra_attributes": map[interface{}]interface{}{
+							"$":            `{"data_type":"dcim.interface","entity":{"Interface":{"device":{"name":"my_dev"},"name":"Gig 2"}},"ingestion_ts":1725552914392208722,"producer_app_name":"diode-agent","producer_app_version":"0.0.1","request_id":"req-id","sdk_name":"diode-sdk-go","sdk_version":"0.1.0","state":1}`,
+							"ingestion_ts": "1725552914392208640",
+						},
+						"id":     "ingest-entity:dcim.interface-1725552914392208722-db0931ec-c119-4702-bd74-4f0bed4e110b",
+						"values": []interface{}{},
+					},
+				},
+				"total_results": 1,
+				"warning":       []interface{}{},
+			}),
+			response: &reconcilerpb.RetrieveIngestionLogsResponse{
+				Logs: []*reconcilerpb.IngestionLog{
+					{
+						DataType:           "dcim.interface",
+						State:              reconcilerpb.State_RECONCILED,
+						RequestId:          "req-id",
+						IngestionTs:        1725552914392208722,
+						ProducerAppName:    "diode-agent",
+						ProducerAppVersion: "0.0.1",
+						SdkName:            "diode-sdk-go",
+						SdkVersion:         "0.1.0",
+						Entity:             &diodepb.Entity{},
+						Error:              nil,
+					},
+				},
+				NextPageToken: "F/JlO7d81QA=",
+			},
+			failCmd:  false,
+			hasError: false,
+		},
+		{
+			name: "filter by timestamp",
+			in:   reconcilerpb.RetrieveIngestionLogsRequest{IngestionTsStart: 1725552914392208639},
+			result: interface{}(map[interface{}]interface{}{
+				"attributes": []interface{}{},
+				"format":     "STRING",
+				"results": []interface{}{
+					map[interface{}]interface{}{
+						"extra_attributes": map[interface{}]interface{}{
+							"$":            `{"data_type":"dcim.interface","entity":{"Interface":{"device":{"name":"my_dev"},"name":"Gig 2"}},"ingestion_ts":1725552914392208722,"producer_app_name":"diode-agent","producer_app_version":"0.0.1","request_id":"req-id","sdk_name":"diode-sdk-go","sdk_version":"0.1.0","state":1}`,
+							"ingestion_ts": "1725552914392208640",
+						},
+						"id":     "ingest-entity:dcim.interface-1725552914392208722-db0931ec-c119-4702-bd74-4f0bed4e110b",
+						"values": []interface{}{},
+					},
+				},
+				"total_results": 1,
+				"warning":       []interface{}{},
+			}),
+			response: &reconcilerpb.RetrieveIngestionLogsResponse{
+				Logs: []*reconcilerpb.IngestionLog{
+					{
+						DataType:           "dcim.interface",
+						State:              reconcilerpb.State_RECONCILED,
+						RequestId:          "req-id",
+						IngestionTs:        1725552914392208722,
+						ProducerAppName:    "diode-agent",
+						ProducerAppVersion: "0.0.1",
+						SdkName:            "diode-sdk-go",
+						SdkVersion:         "0.1.0",
+						Entity:             &diodepb.Entity{},
+						Error:              nil,
+					},
+				},
+				NextPageToken: "F/JlO7d81QA=",
+			},
+			failCmd:  false,
+			hasError: false,
+		},
+		{
+			name: "pagination check",
+			in:   reconcilerpb.RetrieveIngestionLogsRequest{PageToken: "F/Jk/zc08gA="},
+			result: interface{}(map[interface{}]interface{}{
+				"attributes": []interface{}{},
+				"format":     "STRING",
+				"results": []interface{}{
+					map[interface{}]interface{}{
+						"extra_attributes": map[interface{}]interface{}{
+							"$":            `{"data_type":"dcim.interface","entity":{"Interface":{"device":{"name":"my_dev"},"name":"Gig 2"}},"ingestion_ts":1725552914392208722,"producer_app_name":"diode-agent","producer_app_version":"0.0.1","request_id":"req-id","sdk_name":"diode-sdk-go","sdk_version":"0.1.0","state":1}`,
+							"ingestion_ts": "1725552914392208640",
+						},
+						"id":     "ingest-entity:dcim.interface-1725552914392208722-db0931ec-c119-4702-bd74-4f0bed4e110b",
+						"values": []interface{}{},
+					},
+				},
+				"total_results": 1,
+				"warning":       []interface{}{},
+			}),
+			response: &reconcilerpb.RetrieveIngestionLogsResponse{
+				Logs: []*reconcilerpb.IngestionLog{
+					{
+						DataType:           "dcim.interface",
+						State:              reconcilerpb.State_RECONCILED,
+						RequestId:          "req-id",
+						IngestionTs:        1725552914392208722,
+						ProducerAppName:    "diode-agent",
+						ProducerAppVersion: "0.0.1",
+						SdkName:            "diode-sdk-go",
+						SdkVersion:         "0.1.0",
+						Entity:             &diodepb.Entity{},
+						Error:              nil,
+					},
+				},
+				NextPageToken: "F/JlO7d81QA=",
+			},
+			failCmd:  false,
+			hasError: false,
+		},
+		{
+			name: "error parsing extra attributes",
+			in:   reconcilerpb.RetrieveIngestionLogsRequest{PageToken: "F/Jk/zc08gA="},
+			result: interface{}(map[interface{}]interface{}{
+				"attributes": []interface{}{},
+				"format":     "STRING",
+				"results": []interface{}{
+					map[interface{}]interface{}{
+						"extra_attributes": map[interface{}]interface{}{
+							"$":            `"extra":is":"invalid"`,
+							"ingestion_ts": "1725552914392208640",
+						},
+						"id":     "ingest-entity:dcim.interface",
+						"values": []interface{}{},
+					},
+				},
+				"total_results": 1,
+				"warning":       []interface{}{},
+			}),
+			failCmd:  false,
+			hasError: true,
+		},
+		{
+			name: "error parsing timestamp",
+			in:   reconcilerpb.RetrieveIngestionLogsRequest{PageToken: "F/Jk/zc08gA="},
+			result: interface{}(map[interface{}]interface{}{
+				"attributes": []interface{}{},
+				"format":     "STRING",
+				"results": []interface{}{
+					map[interface{}]interface{}{
+						"extra_attributes": map[interface{}]interface{}{
+							"$":            `{"data_type":"dcim.interface","entity":{"Interface":{"device":{"name":"my_dev"},"name":"Gig 2"}},"ingestion_ts":1725552914392208722,"producer_app_name":"diode-agent","producer_app_version":"0.0.1","request_id":"req-id","sdk_name":"diode-sdk-go","sdk_version":"0.1.0","state":1}`,
+							"ingestion_ts": "invalid_ts",
+						},
+						"id":     "ingest-entity:dcim.interface-1725552914392208722-db0931ec-c119-4702-bd74-4f0bed4e110b",
+						"values": []interface{}{},
+					},
+				},
+				"total_results": 1,
+				"warning":       []interface{}{},
+			}),
+			failCmd:  false,
+			hasError: true,
+		},
+		{
+			name:     "error decoding page token",
+			in:       reconcilerpb.RetrieveIngestionLogsRequest{PageToken: "invalid"},
+			failCmd:  false,
+			hasError: true,
+		},
+		{
+			name: "error parsing response json",
+			in:   reconcilerpb.RetrieveIngestionLogsRequest{},
+			result: interface{}(map[interface{}]interface{}{
+				"attributes": []interface{}{},
+				"format":     "STRING",
+				"results": []interface{}{
+					map[interface{}]interface{}{
+						"extra_attributes": map[interface{}]interface{}{
+							"$":            `{"data_type":"dcim.interface","entity":{"Interface":{"device":{"name":"my_dev"},"name":"Gig 2"}},"ingestion_ts":1725552914392208722,"producer_app_name":"diode-agent","producer_app_version":"0.0.1","request_id":"req-id","sdk_name":"diode-sdk-go","sdk_version":"0.1.0","state":1}`,
+							"ingestion_ts": 123,
+						},
+						"id":     "ingest-entity:dcim.interface-1725552914392208722-db0931ec-c119-4702-bd74-4f0bed4e110b",
+						"values": []interface{}{},
+					},
+				},
+				"total_results": 1,
+				"warning":       []interface{}{},
+			}),
+			failCmd:  false,
+			hasError: true,
+		},
+		{
+			name:     "redis error",
+			in:       reconcilerpb.RetrieveIngestionLogsRequest{},
+			failCmd:  true,
+			hasError: true,
 		},
 	}
 	for i := range tests {
@@ -234,6 +472,9 @@ func TestRetrieveLogs(t *testing.T) {
 
 			cmd := redis.NewCmd(ctx)
 			cmd.SetVal(tt.result)
+			if tt.failCmd {
+				cmd.SetErr(errors.New("error"))
+			}
 			mockRedisClient.On("Do", ctx, "FT.SEARCH", "ingest-entity", mock.Anything, "SORTBY", "ingestion_ts", "DESC", "LIMIT", 0, mock.Anything).
 				Return(cmd)
 
