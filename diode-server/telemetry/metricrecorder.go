40a4cb6c465f7f098b56588ce2a9a6355bade4e8
Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>
2025-06-26 14:18:15 +0200

    feat: add metric recorder
    Signed-off-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

---
+++ diode-server/telemetry/metricrecorder.go
@@ -0,0 +1,20 @@
+package telemetry
+
+import (
+	"context"
+	"go.opentelemetry.io/otel/attribute"
+)
+
+// MetricRecorder defines the interface for recording metrics
+type MetricRecorder interface {
+	// RecordCounter increments a counter metric
+	RecordCounter(ctx context.Context, name string, value int64, attrs ...attribute.KeyValue)
+	// RecordHistogram records a value in a histogram metric
+	RecordHistogram(ctx context.Context, name string, value float64, attrs ...attribute.KeyValue)
+	// SetGauge sets the value of a gauge metric
+	SetGauge(ctx context.Context, name string, value int64, attrs ...attribute.KeyValue)
+	// RecordUpDownCounter modifies an up/down counter metric
+	RecordUpDownCounter(ctx context.Context, name string, value int64, attrs ...attribute.KeyValue)
+	// SetServiceInfo sets the service information metric (called once at startup)
+	SetServiceInfo(ctx context.Context, version string)
+}
