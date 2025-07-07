d0f07d190cf4d93ec0f229961d2f31892fb2991f
James Jeffries <jajeffries@users.noreply.github.com>
2025-07-07 09:07:31 +0100

    Adds goose begin and end
    Co-authored-by: Michal Fiedorowicz <mfiedorowicz@netboxlabs.com>

--- diode-server/dbstore/postgres/migrations/00003_add_updated_at_triggers.sql
+++ diode-server/dbstore/postgres/migrations/00003_add_updated_at_triggers.sql
@@ -1,16 +1,20 @@
 -- +goose Up
 
 -- Add trigger to change_sets table
+-- +goose StatementBegin
 CREATE TRIGGER update_change_sets_updated_at
     BEFORE UPDATE ON change_sets
     FOR EACH ROW
     EXECUTE FUNCTION update_updated_at_column();
+-- +goose StatementEnd
 
 -- Add trigger to changes table
+-- +goose StatementBegin
 CREATE TRIGGER update_changes_updated_at
     BEFORE UPDATE ON changes
     FOR EACH ROW
     EXECUTE FUNCTION update_updated_at_column();
+-- +goose StatementEnd
 
 -- +goose Down
 
