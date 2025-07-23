dccce1fb58e101f284245696ebf201675114459d
Luke Tucker <ltucker@netboxlabs.com>
2025-07-23 11:48:04 -0400

    feat: update protobuf to support NetBox v4.3 types

--- diode-server/gen/diode/v1/diodepb/ingester_grpc.pb.go
+++ diode-server/gen/diode/v1/diodepb/ingester_grpc.pb.go
@@ -5,8 +5,8 @@
 // source: diode/v1/ingester.proto
 
 // Generated Code. DO NOT EDIT.
-// Source: NetBox Community v4.2.3
-// Timestamp: 2025-04-10 14:44:18Z
+// Source: NetBox v4.3.4
+// Timestamp: 2025-07-23 01:46:42Z
 
 package diodepb
 
