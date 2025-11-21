ddb0bc9c3d57e92bb7b4b220d9f513360676103a
Benjamin Reed <breed@netboxlabs.com>
2025-11-21 11:43:06 -0500

    feat(redis): add support for specifying username

--- diode-server/ingester/config.go
+++ diode-server/ingester/config.go
@@ -10,6 +10,7 @@ type Config struct {
 	GRPCPort      int    `envconfig:"GRPC_PORT" default:"8081"`
 	RedisHost     string `envconfig:"REDIS_HOST" default:"127.0.0.1"`
 	RedisPort     string `envconfig:"REDIS_PORT" default:"6379"`
+	RedisUsername string `envconfig:"REDIS_USERNAME" default:""`
 	RedisPassword string `envconfig:"REDIS_PASSWORD" required:"true"`
 	RedisStreamDB int    `envconfig:"REDIS_STREAM_DB" default:"1"`
 
